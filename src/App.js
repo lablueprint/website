e3135fcc513025c462aef1ad202f12bb82fc1fe5
leo-liu1 <leoliu1@g.ucla.edu>
2019-10-21 17:44:59 -0700

    Added page not found section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './app/Pages/HomePage'
 import TeamPage from './app/Pages/TeamPage'
+import PageNotFound from './app/Pages/PageNotFound'
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact={true} path="/" render={HomePage} />
-        <Route exact={true} path="/team" render={TeamPage} />
+      	<Switch>
+        	<Route exact={true} path="/" render={HomePage} />
+        	<Route exact={true} path="/team" render={TeamPage} />
+        	<Route render={PageNotFound} />
+        </Switch>
       </BrowserRouter>
     );
   }
