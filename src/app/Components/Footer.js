657700f95609fae7d7d2069db91da51951494d64
Vincent Cheung <vfhcheung@gmail.com>
2019-11-21 15:11:32 -0800

    Refactor footer

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -1,65 +1,134 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import logo_white from '..//../images/logo_white.png';
 import { FaFacebookF, FaTwitter, FaGithub, FaMediumM } from 'react-icons/fa';
 import '../../App.css';
 
-export default function Footer(props) {
-  return (
-    <div className="container-fluid py-5" style={footerStyle}>
-      <Container>
-        <Row>
-          <Col sm={12} md={4} className="mb-5">
-            <Image
-              src={logo_white}
-              height={30}
-              className="pr-2"
-            />
-            <div className="d-inline-block" style={logoStyle} >blueprint</div>
-          </Col>
-          <Col xs={6} md={2} className="mb-5">
-            <h6 className="mb-3">General</h6>
-            <ul className="nav flex-column">
-              <li className="nav-item pt-1"><a style={linkStyle} href='/'>About</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/team'>Team</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'>Projects</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/contact'>Contact</a></li>
-            </ul>
-          </Col>
-          <Col xs={6} md={2} className="mb-5">
-            <h6 className="mb-3">Students</h6>
-            <ul className="nav flex-column">
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'>Apply</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'>Process</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'>FAQs</a></li>
-            </ul>
-          </Col>
-          <Col xs={6} md={2} className="mb-5">
-            <h6 className="mb-3">Nonprofits</h6>
-            <ul className="nav flex-column">
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'>Apply</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'>Process</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'>FAQs</a></li>
-            </ul>
-          </Col>
-          <Col xs={6} md={2} className="mb-5">
-            <h6 className="mb-3">Social Media</h6>
-            <ul className="nav flex-column">
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'><FaFacebookF className="mr-1"/>Facebook</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'><FaTwitter className="mr-1"/>Twitter</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='https://github.com/lablueprint/'><FaGithub className="mr-1"/>Github</a></li>
-              <li className="nav-item pt-1"><a style={linkStyle} href='/#'><FaMediumM className="mr-1"/>Blog</a></li>
-            </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={4} style={copyrightStyle}>
-            &copy; LA Blueprint 2019
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+const generalLinks = [
+  {
+    linkText: 'About',
+    href: '/',
+  },
+  {
+    linkText: 'Team',
+    href: '/team',
+  },
+  {
+    linkText: 'Projects',
+    href: '/projects',
+  },
+  {
+    linkText: 'Contact',
+    href: '/contact',
+  }
+]
+
+const studentLinks = [
+  {
+    linkText: 'Apply',
+    href: '/',
+  },
+  {
+    linkText: 'Process',
+    href: '/',
+  },
+  {
+    linkText: 'Faqs',
+    href: '/',
+  }
+]
+
+const nonprofitLinks = [
+  {
+    linkText: 'Apply',
+    href: '/',
+  },
+  {
+    linkText: 'Process',
+    href: '/',
+  },
+  {
+    linkText: 'Faqs',
+    href: '/',
+  }
+]
+
+const socialLinks = [
+  {
+    linkText: 'Facebook',
+    href: '/',
+    icon: FaFacebookF,
+  },
+  {
+    linkText: 'Twitter',
+    href: '/',
+    icon: FaTwitter,
+  },
+  {
+    linkText: 'Github',
+    href: 'https://github.com/lablueprint/',
+    icon: FaGithub,
+  },
+  {
+    linkText: 'Blog',
+    href: '/',
+    icon: FaMediumM,
+  }
+]
+
+export default class Footer extends Component {
+
+  generateNavItems(heading, links) {
+    const navItems = links.map((item, i) => (
+      <li key={i} className="nav-item pt-1">
+        <a style={linkStyle} href={item.href}>
+          {item.icon ? <item.icon style={socialIconStyle} className="mr-1"/> : null}
+          {item.linkText}
+        </a>
+      </li>
+    ));
+    return (
+      <Col xs={6} md={2} className="mb-5">
+        <h6 className="mb-3">{heading}</h6>
+        <ul className="nav flex-column">
+          {navItems}
+        </ul>
+      </Col>
+    );
+  }
+
+  render() {
+    const generalNavItems = this.generateNavItems('General', generalLinks);
+    const studentNavItems = this.generateNavItems('Students', studentLinks);
+    const nonprofitNavItems = this.generateNavItems('Nonprofits', nonprofitLinks);
+    const socialNavItems = this.generateNavItems('Social Media', socialLinks);
+
+    return (
+      <div className="container-fluid py-5" style={footerStyle}>
+        <Container>
+          <Row>
+            <Col sm={12} md={4} className="mb-5">
+              <Image
+                src={logo_white}
+                height={30}
+                className="pr-2"
+              />
+              <div className="d-inline-block" style={logoStyle} >blueprint</div>
+            </Col>
+            {generalNavItems}
+            {studentNavItems}
+            {nonprofitNavItems}
+            {socialNavItems}
+          </Row>
+          <Row>
+            <Col md={4} style={copyrightStyle}>
+              &copy; LA Blueprint 2019
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 const logoStyle = {
@@ -86,3 +155,7 @@ const copyrightStyle = {
   color: 'rgba(249,250,252,0.6)',
   fontSize: '0.9em'
 };
+
+const socialIconStyle = {
+  verticalAlign: -2
+}
