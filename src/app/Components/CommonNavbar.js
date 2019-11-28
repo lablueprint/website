7d2b5984528f885f2b3a3a3e29d4507002080471
Vincent Cheung <vfhcheung@gmail.com>
2019-11-27 22:48:47 -0800

    Push navbar margins in to align with the rest of the website

--- src/app/Components/CommonNavbar.js
+++ src/app/Components/CommonNavbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom';
 import logo_white from '..//../images/logo_white.png';
@@ -7,32 +7,36 @@ import '../../App.css';
 
 export default function CommonNavbar(props) {
   return (
-    <Navbar variant="dark" style={navbarStyle} expand='md'>
-      <Fade left>
-        <Navbar.Brand href="/" style={logoStyle}>
-          <img
-            alt=""
-            src={logo_white}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />
-          {' blueprint'}
-        </Navbar.Brand>
-      </Fade>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto nav-link">
-          <Fade right>
-            <Link className="nav-link" to="/" onClick={() => props.setScrollDestination(null)}>Home</Link>
-            <Link className="nav-link" to="/" onClick={() => props.setScrollDestination("whoweare")}>About</Link>
-            <Link className="nav-link" to="/team">Team</Link>
-            <Link className="nav-link" to="/projects">Projects</Link>
-            <Link className="nav-link" to="/contact">Contact</Link>
+    <div className="container-fluid" style={bgStyle}>
+      <Container>
+        <Navbar variant="dark" style={navbarStyle} expand='md'>
+          <Fade left>
+            <Navbar.Brand href="/" style={logoStyle}>
+              <img
+                alt=""
+                src={logo_white}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+              {' blueprint'}
+            </Navbar.Brand>
           </Fade>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto nav-link">
+              <Fade right>
+                <Link className="nav-link" to="/" onClick={() => props.setScrollDestination(null)}>Home</Link>
+                <Link className="nav-link" to="/" onClick={() => props.setScrollDestination("whoweare")}>About</Link>
+                <Link className="nav-link" to="/team">Team</Link>
+                <Link className="nav-link" to="/projects">Projects</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Fade>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </div>
   );
 }
 
@@ -43,18 +47,14 @@ const logoStyle = {
 };
 
 const navbarStyle = {
-  background: '#187be5',
-  position: "static",
-  fontFamily: 'Roboto'
+  position: 'static',
+  fontFamily: 'Roboto',
+  paddingLeft: 0,
+  paddingRight: 0
 };
 
-// const homePageNavbarStyle = {
-//   opacity: 1,
-//   position: 'absolute',
-//   width: '100%'
-// };
-
-// const teamPageNavbarStyle = {
-//   background: '#187be5',
-//   position: "static"
-// };
+const bgStyle = {
+  background: '#187be5',
+  padding: 0,
+  margin: 0
+};
