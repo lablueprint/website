7f40f55352b1a7e4c82847735dfc102bc5b6d7b2
leo-liu1 <leoliu1@g.ucla.edu>
2019-12-12 02:37:48 -0800

    Refactored footer to be stateless component

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -1,8 +1,40 @@
-import React, { Component } from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import logo_white from '..//../images/logo_white.png';
-import { FaFacebookF, FaTwitter, FaGithub, FaMediumM } from 'react-icons/fa';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
+import React from 'react';
+// eslint-disable-next-line object-curly-newline
+import { Container, Row, Col, Image } from 'react-bootstrap';
+// import { FaFacebookF, FaTwitter, FaGithub, FaMediumM } from "react-icons/fa";
+import { FaGithub } from 'react-icons/fa';
+import LogoWhite from '../../images/logo_white.png';
+
+const logoStyle = {
+  fontFamily: 'Poppins',
+  fontWeight: 'bold',
+  fontSize: '1.25rem',
+  textShadow: 'none',
+};
+
+const footerStyle = {
+  backgroundColor: '#2C3E50',
+  color: 'white',
+  minHeight: '35vh',
+  fontFamily: 'Roboto',
+};
+
+const linkStyle = {
+  fontSize: '0.9em',
+  fontFamily: 'Roboto',
+  color: 'rgba(249,250,252,0.6)',
+};
+
+const copyrightStyle = {
+  color: 'rgba(249,250,252,0.6)',
+  fontSize: '0.9em',
+};
+
+const socialIconStyle = {
+  verticalAlign: -2,
+};
 
 const generalLinks = [
   {
@@ -20,38 +52,38 @@ const generalLinks = [
   {
     linkText: 'Contact',
     href: '/contact',
-  }
+  },
 ];
 
-const studentLinks = [
-  // {
-  //   linkText: 'Apply',
-  //   href: '/',
-  // },
-  // {
-  //   linkText: 'Process',
-  //   href: '/',
-  // },
-  // {
-  //   linkText: 'Faqs',
-  //   href: '/',
-  // }
-];
+// const studentLinks = [
+//   {
+//     linkText: "Apply",
+//     href: "/"
+//   },
+//   {
+//     linkText: "Process",
+//     href: "/"
+//   },
+//   {
+//     linkText: "Faqs",
+//     href: "/"
+//   }
+// ];
 
-const nonprofitLinks = [
-  // {
-  //   linkText: 'Apply',
-  //   href: '/',
-  // },
-  // {
-  //   linkText: 'Process',
-  //   href: '/',
-  // },
-  // {
-  //   linkText: 'Faqs',
-  //   href: '/',
-  // }
-];
+// const nonprofitLinks = [
+//   {
+//     linkText: "Apply",
+//     href: "/"
+//   },
+//   {
+//     linkText: "Process",
+//     href: "/"
+//   },
+//   {
+//     linkText: "Faqs",
+//     href: "/"
+//   }
+// ];
 
 const socialLinks = [
   // {
@@ -76,86 +108,54 @@ const socialLinks = [
   // }
 ];
 
-export default class Footer extends Component {
-
-  generateNavItems(heading, links) {
-    const navItems = links.map((item, i) => (
-      <li key={i} className="nav-item pt-1">
-        <a style={linkStyle} href={item.href}>
-          {item.icon ? <item.icon style={socialIconStyle} className="mr-1"/> : null}
-          {item.linkText}
-        </a>
-      </li>
-    ));
-    return (
-      <Col xs={6} md={2} className="mb-5">
-        <h6 className="mb-3">{heading}</h6>
-        <ul className="nav flex-column">
-          {navItems}
-        </ul>
-      </Col>
-    );
-  }
-
-  render() {
-    const generalNavItems = this.generateNavItems('General', generalLinks);
-    // const studentNavItems = this.generateNavItems('Students', studentLinks);
-    // const nonprofitNavItems = this.generateNavItems('Nonprofits', nonprofitLinks);
-    const socialNavItems = this.generateNavItems('Social Media', socialLinks);
-
-    return (
-      <div className="container-fluid py-5" style={footerStyle}>
-        <Container>
-          <Row>
-            <Col sm={12} md={4} className="mb-5">
-              <Image
-                src={logo_white}
-                height={30}
-                className="pr-2"
-              />
-              <div className="d-inline-block" style={logoStyle} >blueprint</div>
-            </Col>
-            {generalNavItems}
-            {/*studentNavItems*/}
-            {/*nonprofitNavItems*/}
-            {socialNavItems}
-          </Row>
-          <Row>
-            <Col md={4} style={copyrightStyle}>
-              &copy; LA Blueprint 2019
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+function generateNavItems(heading, links) {
+  const navItems = links.map((item) => (
+    /* setting key as item is kind of choppy, but each object should be unique
+    (in a list) so i don't think there will be any issues */
+    <li key={JSON.stringify(item)} className="nav-item pt-1">
+      <a style={linkStyle} href={item.href}>
+        {item.icon ? (
+          <item.icon style={socialIconStyle} className="mr-1" />
+        ) : null}
+        {item.linkText}
+      </a>
+    </li>
+  ));
+  return (
+    <Col xs={6} md={2} className="mb-5">
+      <h6 className="mb-3">{heading}</h6>
+      <ul className="nav flex-column">{navItems}</ul>
+    </Col>
+  );
 }
 
-const logoStyle = {
-  fontFamily: 'Poppins',
-  fontWeight: 'bold',
-  fontSize: '1.25rem',
-  textShadow: 'none'
-};
+export default function Footer() {
+  const generalNavItems = generateNavItems('General', generalLinks);
+  // const studentNavItems = generateNavItems('Students', studentLinks);
+  // const nonprofitNavItems = generateNavItems('Nonprofits', nonprofitLinks);
+  const socialNavItems = generateNavItems('Social Media', socialLinks);
 
-const footerStyle = {
-  backgroundColor: '#2C3E50',
-  color: 'white',
-  minHeight: '35vh',
-  fontFamily: 'Roboto'
-};
-
-const linkStyle = {
-  fontSize: '0.9em',
-  fontFamily: 'Roboto', 
-  color: 'rgba(249,250,252,0.6)'
-};
-
-const copyrightStyle = {
-  color: 'rgba(249,250,252,0.6)',
-  fontSize: '0.9em'
-};
-
-const socialIconStyle = {
-  verticalAlign: -2
-};
+  return (
+    <div className="container-fluid py-5" style={footerStyle}>
+      <Container>
+        <Row>
+          <Col sm={12} md={4} className="mb-5">
+            <Image src={LogoWhite} height={30} className="pr-2" />
+            <div className="d-inline-block" style={logoStyle}>
+              blueprint
+            </div>
+          </Col>
+          {generalNavItems}
+          {/* studentNavItems */}
+          {/* nonprofitNavItems */}
+          {socialNavItems}
+        </Row>
+        <Row>
+          <Col md={4} style={copyrightStyle}>
+            &copy; LA Blueprint 2019
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
