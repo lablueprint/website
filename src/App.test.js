f84875f441caa00f1b3499c1123fbc02bbffdde4
leo-liu1 <leoliu1@g.ucla.edu>
2020-01-06 04:44:01 -0800

    deleted some images, fixed App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
 // eslint-disable-next-line no-undef
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>, document.getElementById('root'),
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
