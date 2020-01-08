b7e905c8cf56bf4daaa7afa1b0caf1bb93fc5327
leo-liu1 <leoliu1@g.ucla.edu>
2020-01-08 04:40:18 -0800

    added scroll to top functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -9,6 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
+    {/* scrolls window to the top of the screen when changing routes */}
+    <Route component={() => { window.scrollTo(0, 0); return null; }} />
     <App />
   </BrowserRouter>, document.getElementById('root'),
 );
