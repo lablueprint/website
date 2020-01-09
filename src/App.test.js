e68c15d3422203d33f380c2e5cd69e20af2a225e
leo-liu1 <leoliu1@g.ucla.edu>
2020-01-09 01:59:07 -0800

    fixed App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import App from './App';
+
+const history = createBrowserHistory();
+
+function onMount() {
+  // do nothing since this is for google analytics
+}
 
 // eslint-disable-next-line no-undef
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>, div,
+    <Router history={history}>
+      <App onMount={onMount} />
+    </Router>, div,
   );
   ReactDOM.unmountComponentAtNode(div);
 });
