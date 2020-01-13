9035fb52cc0f9ba0cedd5014d49f27e25cee124a
leo-liu1 <leoliu1@g.ucla.edu>
2020-01-13 02:45:14 -0800

    fixed error when running App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// to get rid of warning, 'Error: Not implemented: window.scrollTo'
+Object.defineProperty(window, 'scrollTo', { value: () => {}, writable: true });
+
 // eslint-disable-next-line no-undef
 it('renders without crashing', () => {
   const div = document.createElement('div');
