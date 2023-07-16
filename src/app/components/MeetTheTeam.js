e478bdae2c02c17ffaf7c4a7656ca4e4321cba50
Siddharth Nandy <sidd.nandy@gmail.com>
2023-07-16 15:58:50 -0700

    cleanup

--- src/app/components/MeetTheTeam.js
+++ src/app/components/MeetTheTeam.js
@@ -3,27 +3,28 @@ import { Col, Container, Row } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 
 // exec
-
-import IsaacWen from '../assets/images/team/isaac_wen.png';
-
-// PLs
 import JamesHe from '../assets/images/team/james_he.png';
 import JerryXu from '../assets/images/team/jerry_xu.png';
-import SarahChang from '../assets/images/team/sarah_chang.png';
 import SoliaValentine from '../assets/images/team/solia_valentine.png';
+import ErinKwon from '../assets/images/team/erin_kwon.png';
+import SiddharthNandy from '../assets/images/team/siddharth_nandy.png';
+import SarahChang from '../assets/images/team/sarah_chang.png';
+import GalenHeuer from '../assets/images/team/galen_heuer.png';
 
+// PLs
 import AaronShi from '../assets/images/team/aaron_shi.png';
-import AlanNguyen from '../assets/images/team/alan_nguyen.png';
 import AngelaLing from '../assets/images/team/angela_ling.png';
+import CarolineDebBaruah from '../assets/images/team/caroline_debBaruah.png';
+import IsaacWen from '../assets/images/team/isaac_wen.png';
+import JeffLu from '../assets/images/team/jeff_lu.png';
+import RyanYeo from '../assets/images/team/ryan_yeo.png';
+
+import AlanNguyen from '../assets/images/team/alan_nguyen.png';
 import AngelaHao from '../assets/images/team/angela_hao.png';
 import AngelaTan from '../assets/images/team/angela_tan.png';
 import AyubAli from '../assets/images/team/ayub_ali.png';
-import CarolineDebBaruah from '../assets/images/team/caroline_debBaruah.png';
 import DanielKao from '../assets/images/team/daniel_kao.png';
 import DiyaIndoliya from '../assets/images/team/diya_indoliya.png';
-import ErinKwon from '../assets/images/team/erin_kwon.png';
-import GalenHeuer from '../assets/images/team/galen_heuer.png';
-import JeffLu from '../assets/images/team/jeff_lu.png';
 import JimmyTang from '../assets/images/team/jimmy_tang.png';
 import JiyeonHan from '../assets/images/team/jiyeon_han.png';
 import JoyceChen from '../assets/images/team/joyce_chen.png';
@@ -33,8 +34,6 @@ import KennyWan from '../assets/images/team/kenny_wan.png';
 import LauraLu from '../assets/images/team/laura_lu.png';
 import MeganTieu from '../assets/images/team/megan_tieu.png';
 import RyanKim from '../assets/images/team/ryan_kim.png';
-import RyanYeo from '../assets/images/team/ryan_yeo.png';
-import SiddharthNandy from '../assets/images/team/siddharth_nandy.png';
 
 import MemberProfile from './MemberProfile';
 
