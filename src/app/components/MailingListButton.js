4252c4b63256ab8d20cf6c4498232ddffaade658
Siddharth Nandy <sidd.nandy@gmail.com>
2023-09-21 02:49:00 -0700

    Add dates to timeline

--- src/app/components/MailingListButton.js
+++ src/app/components/MailingListButton.js
@@ -8,10 +8,10 @@ export default function MailingListButton() {
     <SectionContainer className="mailing-list-button">
       <Container className="mailing-list-button-container mb-5 text-center">
         {/* <div className="mailing-list-button-text">
-          Applications are now open! The deadline is October 16th, 11:59 PM PST.
+          Applications are now open! The deadline is October 6th, 11:59 PM PST.
         </div>
-        <Button className="raised-button blue" href="https://airtable.com/shrlSddk7PCH8Cc05">
-          Check out our applications
+        <Button className="raised-button blue" href="[INSERT APPLICATION LINK]">
+          2023-24 Application
         </Button> */}
         <div className="mailing-list-button-text">
           Fill out our interest form to get an email when applications open!
