8eda1633c983c89dac9f2f974716c3a49ee9f131
Ryan Yeo <ryandc.yeo@gmail.com>
2024-09-28 16:19:36 -0700

    updated for students

--- src/app/pages/StudentPage.js
+++ src/app/pages/StudentPage.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Fade from 'react-reveal';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import Fade from "react-reveal";
+import { Button } from "react-bootstrap";
 
-import PageHeader from '../components/PageHeader';
-import ApplicationProcess from '../components/ApplicationProcess';
-import StudentFAQ from '../components/StudentFAQ';
-import MailingListButton from '../components/MailingListButton';
+import PageHeader from "../components/PageHeader";
+import ApplicationProcess from "../components/ApplicationProcess";
+import StudentFAQ from "../components/StudentFAQ";
+import MailingListButton from "../components/MailingListButton";
 
-import studentImage from '../assets/images/headers/for-students-23-24.png';
+import studentImage from "../assets/images/headers/for-students-23-24.png";
 
-import SectionSubtitle from '../components/SectionSubtitle';
+import SectionSubtitle from "../components/SectionSubtitle";
 
 export default function StudentPage() {
   return (
@@ -20,22 +20,27 @@ export default function StudentPage() {
         render={() => (
           <>
             <SectionSubtitle>
-              Joining Blueprint is a great way to give back to your community. Help us build our
-              applications and work with nonprofits!
+              Joining Blueprint is a great way to give back to your community.
+              Help us build our applications and work with nonprofits!
             </SectionSubtitle>
-            {/* <SectionSubtitle className="sign-up mb-3">
-              Applications are currently open! The deadline is October 6th, 11:59 PM PST.
-            </SectionSubtitle>
-            <Button className="outline-white" size="md" href="https://airtable.com/appxZCTFmfmTz8hR0/shrLYrIOpBsVNZGtV">
-              Check out our applications
-            </Button> */}
             <SectionSubtitle className="sign-up mb-3">
+              Applications are currently open! The deadline is Friday October
+              4th, 11:59 PM PST.
+            </SectionSubtitle>
+            <Button
+              className="outline-white"
+              size="md"
+              href="https://airtable.com/appY8mDtwETX2LcL5/shrgwdaqOvjbi8FbJ"
+            >
+              Check out our applications
+            </Button>
+            {/* <SectionSubtitle className="sign-up mb-3">
               Applications are currently closed. Fill out our interest form for more updates on
               our other initiatives!
             </SectionSubtitle>
             <Button className="outline-white" href="https://airtable.com/appNXPLX16HDPNLLY/shrjFzXLVJ0vbGCSy">
               2023-24 Interest Form
-            </Button>
+            </Button> */}
           </>
         )}
         image={studentImage}
