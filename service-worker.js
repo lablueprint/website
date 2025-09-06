fdb1aae1216a68d27e16389f36f4b0476ed88b4f
Victoria Choi <121906670+vicchois@users.noreply.github.com>
2025-09-05 21:06:20 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00a625bcdaf3c124ac2d28513c6e9a43.js"
+  "/precache-manifest.5356144e7cff03f4887f057890ea6fa2.js"
 );
 
 self.addEventListener('message', (event) => {
